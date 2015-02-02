2c76e89d4697fd95e02f1b87c40be4d973ecd275
AndiOxidant <andi.oxidant@noname-media.com>
2015-02-02 08:18:31 +0100

    Changed Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 /*
- * grunt-doxit
- * https://github.com/AndiOxidant/grunt-doxit
+ * grunt-doxydoc
+ * https://github.com/AndiOxidant/grunt-doxydoc
  *
  * Copyright (c) 2014 Andi Oxidant
  * Licensed under the MIT license.
@@ -29,7 +29,7 @@ module.exports = function(grunt) {
     },
 
     // Configuration to be run (and then tested).
-    doxit: {
+    doxydoc: {
       default_options: {
         options: {
           beautifyCode: true
@@ -62,7 +62,7 @@ module.exports = function(grunt) {
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'doxit', 'mochaTest']);
+  grunt.registerTask('test', ['clean', 'doxydoc', 'mochaTest']);
 
   // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);
