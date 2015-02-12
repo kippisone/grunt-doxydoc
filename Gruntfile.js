1f1a9a030a3ff22200f72480a53fdc21a16ce204
AndiOxidant <andi.oxidant@noname-media.com>
2015-02-12 18:13:46 +0100

    Initial import

---
+++ Gruntfile.js
@@ -0,0 +1,70 @@
+/*
+ * grunt-doxydoc
+ * https://github.com/AndiOxidant/grunt-doxydoc
+ *
+ * Copyright (c) 2014 Andi Oxidant
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js',
+        '<%= nodeunit.tests %>'
+      ],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+
+    // Before generating any new files, remove any previously-created files.
+    clean: {
+      tests: ['tmp']
+    },
+
+    // Configuration to be run (and then tested).
+    doxydoc: {
+      default_options: {
+        options: {
+          beautifyCode: true
+        },
+        files: {
+          'tmp/': ['test/src/**']
+        }
+      }
+    },
+
+    // Unit tests.
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec'
+        },
+        src: ['test/*.spec.js']
+      }
+    }
+
+  });
+
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-mocha-test');
+
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
+  grunt.registerTask('test', ['clean', 'doxydoc', 'mochaTest']);
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['jshint', 'test']);
+
+};
---
+++ test/src/module2.js
@@ -0,0 +1,49 @@
+/**
+ * Module II
+ *
+ * This is a second test module
+ */
+module.exports = function() {
+    'use strict';
+
+    /**
+     * Module II constructor
+     * @method Module2
+     */
+    var Module2 = function() {
+
+    };
+
+    /**
+     * Foo function
+     * @method foo
+     * @param {Object} arg1 First argument
+     * @returns {String} Returns "foo"
+     */
+    Module2.prototype.foo = function(arg1) {
+        return 'foo';
+    };
+
+    /**
+     * Bar function (Private)
+     * @method bar
+     * @private
+     * @param {Object} arg1 First argument
+     * @returns {String} Returns "bar"
+     */
+    Module2.prototype.bar = function(arg1) {
+        return 'bar';
+    };
+
+    /**
+     * Baz function (Protected)
+     * @method baz
+     * @protected
+     * @param {Object} arg1 First argument
+     * @returns {String} Returns "baz"
+     */
+    Module2.prototype.baz = function(arg1) {
+        return 'baz';
+    };
+    
+};
